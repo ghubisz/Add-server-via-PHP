8d68c9d1cbfae780eb74c77b63055858bf52e100
ghubisz <gab.op.pl@op.pl>
2023-03-01 22:09:23 +0100

    commit

---
+++ redirect.php
@@ -0,0 +1,14 @@
+
+<?php
+session_start();
+if($_SESSION['category']=='advertiser')
+{
+    header("location: advertiser/advertiserpanel.php");
+}
+else
+{
+    header("location: client/clientpanel.php");
+
+}
+
+?>
